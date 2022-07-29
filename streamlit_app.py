af6052f098ebab9edf4d5bfe5449101ff48017a8
kovisha <53814905+kovisha@users.noreply.github.com>
2022-07-29 12:27:58 +0530

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ streamlit.text('Hard-Boiled free range egg')
 
 streamlit.header('Build your own fruit smoothie')
 
-my_fruit_list = pandas.read_csv('https://uni-lab-files/s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
