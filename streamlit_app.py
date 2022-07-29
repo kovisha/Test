0a0f0dcbfb3739b3ce8c020fe0db749cc60d9940
kovisha <53814905+kovisha@users.noreply.github.com>
2022-07-29 12:37:16 +0530

    Update streamlit_app.py
    Updated index with fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ streamlit.text('Hard-Boiled free range egg')
 streamlit.header('Build your own fruit smoothie')
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+streamlit.dataframe(my_fruit_list)
